2b35cfcef9d77c78963b22fa6072b522994b5756
mikereiddigital <mike.reid@justice.gov.uk>
2024-11-04 15:47:25 +0000

    test

--- scripts/failed-issues-report/send-report-via-slack.sh
+++ scripts/failed-issues-report/send-report-via-slack.sh
@@ -1,28 +1,26 @@
-# This bash script sends the contents of a json file to the modernisation platform slack channel.
-# It is dependent on the json file being generated in the script get-failed-issues. Otherwise no report is sent.
+# This bash script sends the contents of the slack_message.son file to the modernisation platform slack channel.
+# It is dependent on the successful creation of the slack_message.json file. Otherwise no report is sent.
 
 #!/bin/bash
 
-# Ensure the script is called with the webhook URL and payload file
+# Ensure the script is called with the webhook URL and json file. Otherwise exit.
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 SLACK_WEBHOOK_URL PAYLOAD_FILE"
   exit 1
 fi
 
-# Assign variables for webhook URL and payload file
+# Variables for the two inputs. 
 SLACK_WEBHOOK_URL="$1"
 PAYLOAD_FILE="$2"
 
-# Check if the payload file exists and is valid JSON
+# Validate the json file. If it fails then exit.
 if ! jq empty "$PAYLOAD_FILE" > /dev/null 2>&1; then
   echo "Error: $PAYLOAD_FILE is not valid JSON or does not exist."
   exit 1
 fi
 
-# Read the JSON payload from the file
+# Reads the payload and adds it to a variable. Then sends to slack the variable as the message data.
 PAYLOAD=$(cat "$PAYLOAD_FILE")
-
-# Send the payload to Slack
 response=$(curl -s -X POST -H 'Content-type: application/json' --data "$PAYLOAD" "$SLACK_WEBHOOK_URL")
 
 # Check for a successful response
